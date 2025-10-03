cec688917c80a47caba5a325c6ef973f5a9f25f4
carvilsi <carvilsi.mail.list@gmail.com>
2025-10-03 12:45:04 +0200

    first commit, splitting raw file from flipperZero

---
+++ splipt_raw_wifi_map_data.py
@@ -0,0 +1,23 @@
+import sys
+import os
+
+if len(sys.argv) == 1:
+    raise Exception("You must provide a FlipperZero WiFi Map csv file")
+
+working_folder = "flpprzr_wfm_workspace" if len(sys.argv) == 2 else sys.argv[2]
+
+if not os.path.exists(working_folder):
+        os.makedirs(working_folder)
+
+file_tmp = None
+
+with open(sys.argv[1], "r") as file:
+    for line in file:
+        if line.startswith("#####"):
+            data_date = line.split(" ")
+            file_name = working_folder + "/" + data_date[1].replace(":", "_") + "-flpprzr_wfm.csv"
+            print("Creating: " + file_name)
+            file_tmp = open(file_name, "w")
+        else:
+            file_tmp.write(line)
+
