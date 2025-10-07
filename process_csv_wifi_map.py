6a898a3ff47bb2d8acbb2608f11769653fc03492
carvilsi <carvilsi.mail.list@gmail.com>
2025-10-07 11:10:14 +0200

    adding script for linter and formatter based on Ruff and formatted

--- process_csv_wifi_map.py
+++ process_csv_wifi_map.py
@@ -3,10 +3,10 @@ import sys
 import matplotlib.pyplot as plt
 
 if len(sys.argv) == 1:
-    raise Exception('You must provide a FlipperZero WiFi Map csv file')
+    raise Exception("You must provide a FlipperZero WiFi Map csv file")
 
 with open(sys.argv[1]) as file_obj:
-    reader = csv.DictReader(file_obj, delimiter=';')
+    reader = csv.DictReader(file_obj, delimiter=";")
 
     time_arr = []
     dist_arr = []
@@ -14,29 +14,30 @@ with open(sys.argv[1]) as file_obj:
     closer = 0
 
     for row in reader:
-        ap_hash = row['AP hash']
-        distance = row['Distance (meters)']
-        ap_auth_mode = row['AP auth mode']
-        time = row['Time from start (seconds)']
+        ap_hash = row["AP hash"]
+        distance = row["Distance (meters)"]
+        ap_auth_mode = row["AP auth mode"]
+        time = row["Time from start (seconds)"]
 
         time_arr.append(int(time))
         dist_arr.append(float(distance))
         ap_name_arr.append(ap_hash[5:])
         closer = float(distance)
-        
-    # draw something
-    plt.xlabel('Time since ESP32 is connected to FlipperZero (s)')
-    plt.ylabel('Distance to AP (m)')
 
-    plt.plot(time_arr, dist_arr, '.')
+    # draw something
+    plt.xlabel("Time since ESP32 is connected to FlipperZero (s)")
+    plt.ylabel("Distance to AP (m)")
+
+    plt.plot(time_arr, dist_arr, ".")
     i = 0
-    for x,y in zip(time_arr, dist_arr):
-        plt.annotate(ap_name_arr[i],
-                     (x,y),
-                     textcoords="offset points",
-                     xytext=(0,0), 
-                     ha='right')
+    for x, y in zip(time_arr, dist_arr):
+        plt.annotate(
+            ap_name_arr[i],
+            (x, y),
+            textcoords="offset points",
+            xytext=(0, 0),
+            ha="right",
+        )
         i += 1
 
     plt.show()
-            
--- splipt_raw_wifi_map_data.py
+++ splipt_raw_wifi_map_data.py
@@ -7,7 +7,7 @@ if len(sys.argv) == 1:
 working_folder = "flpprzr_wfm_workspace" if len(sys.argv) == 2 else sys.argv[2]
 
 if not os.path.exists(working_folder):
-        os.makedirs(working_folder)
+    os.makedirs(working_folder)
 
 file_tmp = None
 
@@ -15,10 +15,16 @@ with open(sys.argv[1], "r") as file:
     for line in file:
         if line.startswith("#####"):
             data_date = line.split(" ")
-            file_name = working_folder + "/" + data_date[1].replace(":", "_") + "-flpprzr_wfm.csv"
+            file_name = (
+                working_folder
+                + "/"
+                + data_date[1].replace(":", "_")
+                + "-flpprzr_wfm.csv"
+            )
             print("Creating: " + file_name)
             file_tmp = open(file_name, "w")
-            file_tmp.write("AP hash;Distance (meters);AP auth mode;Time from start (seconds)\n")
+            file_tmp.write(
+                "AP hash;Distance (meters);AP auth mode;Time from start (seconds)\n"
+            )
         else:
             file_tmp.write(line)
-
