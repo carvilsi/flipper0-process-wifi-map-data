6ca7b9d1673cb1523029a4adcbae5ae5bfe4af01
carvilsi <carvilsi.mail.list@gmail.com>
2025-10-13 16:42:12 +0200

    processing FlipperZero CSV file and drawing route to OpenStreetMap

--- draw.py
+++ draw.py
@@ -5,32 +5,34 @@ df = pd.read_csv("data.csv")
 
 df.dropna(
     axis=0,
-    how='any',
+    how="any",
     # thresh=None,
     subset=None,
-    inplace=True
+    inplace=True,
 )
 
-color_scale = [(0, 'orange'), (1,'red')]
+color_scale = [(0, "orange"), (1, "red")]
 
-# fig = px.scatter_mapbox(df, 
-fig = px.scatter_map(df, 
-                        lat="Lat", 
-                        lon="Long", 
-                        hover_name="Address", 
-                        hover_data=["Address", "Listed"],
-                        # color="Listed",
-                        # color_continuous_scale=color_scale,
-                        # size="Listed",
-                        opacity=1,
-                        zoom=16, 
-                        height=800,
-                        width=800,
-                        map_style="carto-darkmatter-nolabels")
-                        # map_style="carto-voyager-nolabels")
-                        # map_style="carto-darkmatter")
-                        # map_style="dark")
-                        # map_style="open-street-map")
+# fig = px.scatter_mapbox(df,
+fig = px.scatter_map(
+    df,
+    lat="Lat",
+    lon="Long",
+    hover_name="Address",
+    hover_data=["Address", "Listed"],
+    # color="Listed",
+    # color_continuous_scale=color_scale,
+    # size="Listed",
+    opacity=1,
+    zoom=16,
+    height=800,
+    width=800,
+    map_style="carto-darkmatter-nolabels",
+)
+# map_style="carto-voyager-nolabels")
+# map_style="carto-darkmatter")
+# map_style="dark")
+# map_style="open-street-map")
 
 # fig.update_layout(mapbox_style="open-street-map")
 # fig.update_layout(mapbox_style="satellite")
