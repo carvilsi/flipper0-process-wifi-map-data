8642260b81d37976c22e2bda062ff17a1ca18710
carvilsi <carvilsi.mail.list@gmail.com>
2025-10-23 13:31:34 +0200

    adds script to drawing circles on map

---
+++ drawing_circle_on_map.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
+from math import radians, cos, sin, asin, sqrt, pi
+
+def generate_circle_points(lat, lon, radius, name):
+    # parameters
+    N = 360 # number of discrete sample points to be generated along the circle
+    
+    # generate points
+    circle_lats, circle_lons = [], []
+    circle_names = []
+    for k in range(N):
+        # compute
+        angle = pi*2*k/N
+        dx = radius*cos(angle)
+        dy = radius*sin(angle)
+        circle_lats.append(lat + (180/pi)*(dy/6378137))
+        circle_lons.append(lon + (180/pi)*(dx/6378137)/cos(lat*pi/180))
+        circle_names.append(name)
+    circle_lats.append(circle_lats[0])
+    circle_lons.append(circle_lons[0])
+    return (circle_lats, circle_lons, circle_names)
+
+def draw_circle(circle_lats, circle_lons, circle_names, fig):
+    fig.add_trace(go.Scattermap(
+        lat=circle_lats,
+        lon=circle_lons,
+        text=circle_names,
+        mode='lines',
+        marker=go.scattermap.Marker(
+            size=1,
+            color="BlueViolet",
+        ),
+    ))
+
+df = pd.read_csv("data.csv")
+
+fig = px.scatter_map(
+        df,
+        lon="Long",
+        lat="Lat",
+        hover_name="Address", 
+        hover_data=["Address", "Listed"],
+        zoom=16,
+        width=800,
+        height=800,
+        )
+
+
+for i in range(0,len(df)):
+    (circle_lats, circle_lons, circle_names) = generate_circle_points(df.Lat[i], df.Long[i], 5, df.Address[i])
+    draw_circle(circle_lats, circle_lons, circle_names, fig)
+
+fig.update_layout(
+        map_style="carto-darkmatter-nolabels",
+        )
+
+fig.show()
